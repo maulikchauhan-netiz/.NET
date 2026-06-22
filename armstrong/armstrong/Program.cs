e75a4444a8fb03d6864bc086acd4fe2c78d2643a
maulik chauhan <maulik.rajput0001@gmail.com>
2026-06-22 19:49:33 +0530

    armstrong code

---
+++ armstrong/armstrong/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, rem, temp, sum = 0;
+            Console.Write("enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+            temp = num;
+            while (num > 0)
+            {
+                rem = num % 10;
+                sum = sum + (rem * rem * rem);
+                num = num / 10;
+            }
+            if (temp == sum)
+            {
+                Console.Write("number is armstrong");
+
+            }
+            else
+            {
+                Console.Write("number is not armstrong ");
+            }
+
+
+        }
+    }
+}
