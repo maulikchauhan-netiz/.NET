25bca4d67cb368322ddd262d5423bf130e883cca
maulik chauhan <maulik.rajput0001@gmail.com>
2026-06-22 19:39:30 +0530

    pallendrome code

---
+++ p5/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace p5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str, rev = "";
+
+            Console.Write("Enter String: ");
+            str = Console.ReadLine();
+
+            int l = str.Length - 1;
+
+            for (int i = l; i >= 0; i--)
+            {
+                rev = rev + str[i];
+            }
+
+            if (rev == str)
+            {
+                Console.WriteLine("String is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("String is not palindrome");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
