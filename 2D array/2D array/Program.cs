67e1ca942413287de0436a2d57ef109d0ecba736
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-06 20:23:37 +0530

    .net code

---
+++ 2D array/2D array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr =
+            {
+                {50,20,60 },
+                {50,20,80 },
+                {80,50,70 }
+
+            };
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Student:-", (i + 1) + "Marks:-");
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(arr[i , j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
+
+
