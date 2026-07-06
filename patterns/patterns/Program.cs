0b22ccf220511ee365bf108b741bae8e5c941e0c
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-06 20:12:48 +0530

    .net code

---
+++ patterns/patterns/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, k;
+            char ch = 'A';
+
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("====================================");
+
+            for (i = 1; i <= 4; i++)
+            {
+                ch = 'A';
+                for (j = 3; j >= i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write(ch + " ");
+                    ch++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("====================================");
+
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = 5; j >= i; j--)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
