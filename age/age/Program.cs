67eb7e15e0e21e7452197546b11ec4085da272e5
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-06 19:37:59 +0530

    .net code

---
+++ age/age/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AgeGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 0)
+            {
+                Console.WriteLine("invalid age");
+            }
+            else if (age <= 12)
+            {
+                Console.WriteLine("Child");
+            }
+            else if (age <= 19)
+            {
+                Console.WriteLine("Teenager");
+            }
+            else if (age <= 59)
+            {
+                Console.WriteLine("Adult");
+            }
+            else
+            {
+                Console.WriteLine("Senior citizen");
+            }
+        }
+    }
+}
