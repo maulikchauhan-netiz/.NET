32d5ff964edc9bbbbf1a348416b3ecb6dcf8ca4a
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-10 09:02:08 +0530

    .net code

---
+++ constructor_and_destructor/constructor_and_destructor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class ResourceManager
+{
+    public ResourceManager()
+    {
+        Console.WriteLine("resources initialized constructor invoked");
+    }
+    ~ResourceManager()
+    {
+        Console.WriteLine("resources released(destructor invoked");
+        Console.ReadKey();
+
+
+    }
+}
+class Test
+{
+    public static void Main(string[]args)
+    {
+        ResourceManager rm = new ResourceManager();
+      
+    }
+}
+    
