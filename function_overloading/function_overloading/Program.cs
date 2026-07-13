f3db2304ef2affa64a18c61896690077564995ee
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-13 11:15:39 +0530

    .net code

---
+++ function_overloading/function_overloading/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class cal
+{
+    public static int add(int a,int b)
+    {
+        return a + b;
+    }
+    public static float add(float a,float b)
+    {
+        return a + b;
+    }
+}
+public class testmember
+{
+    public static void Main()
+    {
+        Console.WriteLine(cal.add(12, 22));
+        Console.WriteLine(cal.add(12.4f, 22.3f));
+    }
+}
