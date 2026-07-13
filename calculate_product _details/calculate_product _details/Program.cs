fee57858588f037681f5d78d0398fe1474150bd7
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-13 10:45:01 +0530

    .net code

---
+++ calculate_product _details/calculate_product _details/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+public class calculate_product_details
+{
+    public string prodName;
+    public double prodprice;
+    public int prodquantity;
+
+    public calculate_product_details(string name,double price,int quantity)
+    {
+        prodName = name;
+        prodprice = price;
+        prodquantity = quantity;
+    }
+    public void Display()
+    {
+        double totalbill=prodprice*prodquantity;
+        Console.WriteLine("product name : " + prodName);
+        Console.WriteLine("price : " + prodprice);
+        Console.WriteLine("quantity : " + prodquantity);
+        Console.WriteLine("total bill : " + totalbill);
+        Console.WriteLine("==============================");
+
+    }
+}
+class test
+{
+    public static void Main(string[]args)
+    {
+        Console.WriteLine("=========product details========");
+        calculate_product_details p1 = new calculate_product_details("laptop", 450000, 1);
+        p1.Display();
+    }
+}
---
+++ operator_overload/operator_overload/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace operator_overload
+{
+    class class1
+    {
+        public int rno;
+        public string name;
+        public class1(int n,string nm)
+        {
+            rno = n;
+            name = nm;
+        }
+        public static class1 operator + (class1 c1)
+        {
+            c1.rno = c1.rno + 100;
+            c1.name = "hello" + c1.name;
+            return c1;
+        }
+        public void print()
+        {
+            Console.WriteLine("roll no = " + rno);
+            Console.WriteLine("name =" + name);
+        }
+        static void Main(string[] args)
+        {
+            class1 calc = new class1 ( 10 , "hello" );
+            Console.WriteLine("\n--------------------before operator overloading------------------------ ");
+            calc.print();
+            calc = +calc;
+            Console.WriteLine("\n--------------------after operator overloading------------------------ ");
+            calc.print();
+            Console.ReadKey();
+        }
+             
+    }
+}
