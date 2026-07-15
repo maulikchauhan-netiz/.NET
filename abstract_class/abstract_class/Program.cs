9e790e8e67bb0f158b16a80930b6a5dc3655a1d2
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-15 18:52:22 +0530

    .NET code

---
+++ abstract_class/abstract_class/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace abstract_class
+{
+    public abstract class vehicals
+    {
+        public abstract void startengine();
+    }
+    public class car : vehicals
+    {
+        public override void startengine()
+        {
+            Console.WriteLine("car engine started");
+        }
+    }
+    public class motorcycle : vehicals
+    {
+        public override void startengine()
+        {
+            Console.WriteLine("motorcycle engine is started");
+        }
+    }
+    public class test
+    {
+        public static void Main()
+        {
+            vehicals myvehicals;
+            myvehicals = new car();
+            myvehicals.startengine();
+
+            myvehicals=new motorcycle();
+            myvehicals.startengine();
+
+        }
+    }
+}
