fc0818fa2e25383135bc0ef9aeaf13cf54eaf83d
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-23 20:32:17 +0530

    .net code

---
+++ finally_block/finally_block/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UseOfFinallyBlock
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            String[] subject = { "java", "python", ".net", "c++" };
+            Console.WriteLine("enter array index which you want to access");
+            int index = Convert.ToInt16(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("subject = " + subject[index]);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("invalid array index");
+            }
+            finally
+            {
+                index = 0;
+                Console.WriteLine("finally block executed");
+                Console.WriteLine("index reset to {0}", index);
+            }
+        }
+    }
+}
