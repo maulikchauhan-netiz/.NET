9f09d4b62cff96db32735ea0c87b48c10a93a721
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-23 20:10:27 +0530

    .net code

---
+++ divide_by_zero_exception/divide_by_zero_exception/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Divide_by_zero_exception
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("enter the value of a and b");
+            int a = Convert.ToInt16(Console.ReadLine());
+            int b = Convert.ToInt16(Console.ReadLine());
+            int result = 0;
+            try
+            {
+                result = a / b;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("number cannot be divide by zero!!!!!");
+            }
+            finally
+            {
+                Console.WriteLine("result:{0}", result);
+            }
+        }
+
+    }
+}
