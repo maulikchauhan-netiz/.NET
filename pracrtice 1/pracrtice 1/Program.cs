e373f393c315f664bc453de0b4c6c65118e958b1
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-26 08:04:35 +0530

    .net code

---
+++ pracrtice 1/pracrtice 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace program
+{
+    class employee
+    {
+        private string Name;
+        public string name
+        {
+            get
+            {
+                return Name;
+            }
+            set
+                {
+                Name = value;
+            }
+        }
+    }
+
+    class Test
+    {
+        public static void Main(string[] args)
+        {
+            employee e1 = new employee();
+            e1.name = "Maulik";
+            Console.WriteLine("Name is: " + e1.name);
+            Console.ReadKey();
+        }
+    }
+}
