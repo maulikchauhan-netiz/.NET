087a44f5c70f92c6579b293f7f4d4e72053c55ac
maulik chauhan <maulik.rajput0001@gmail.com>
2026-08-03 20:53:26 +0530

    .net code

---
+++ security_manager/security_manager/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class employee
+{
+    private string name;
+
+    public string n1
+    {
+        get
+        {
+            return name;
+
+        }
+        set
+        {
+            name = value;
+        }
+    }
+}
+class test
+{
+    static void Main (string[] args)
+    {
+        employee e1 = new employee();
+        e1.n1 = "maulik";
+        Console.WriteLine ("employee name is : "+e1.n1);
+    }
+}
