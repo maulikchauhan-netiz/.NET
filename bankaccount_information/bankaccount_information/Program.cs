167a8ead6d70fb576da2990831b2c8a8ecaf29e6
maulik chauhan <maulik.rajput0001@gmail.com>
2026-08-03 21:00:22 +0530

    .net code

---
+++ bankaccount_information/bankaccount_information/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class bankaccount
+{
+    private double balance;
+
+    public double b1
+    {
+        get
+        {
+            return balance;
+
+        }
+        set
+        {
+            if (value > 0)
+            {
+                balance = value;
+            }
+            else
+            {
+                Console.WriteLine("invalid amount");
+            }
+        }
+    }
+}
+class test
+{
+    static void Main(string[] args)
+    {
+        bankaccount e1 = new bankaccount();
+        e1.b1 = 0.0;
+        Console.WriteLine("employee name is : " + e1.b1);
+    }
+}
